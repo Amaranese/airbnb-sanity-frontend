27d504a05fb14f8bcd73a40c3e733f4d908eb706
Maranes Alejandro <alejandro.maranes@asendia.com>
2022-02-14 10:45:15 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import NavBar from "../components/NavBar"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
